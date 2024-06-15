7595f0469df178b5c22e15f9389f2211bd4e22bd
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-06-15 13:41:30 -0400

    implement home screen

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,22 @@
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Entypo, FontAwesome5 } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: 'black',
+      }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: 'Home',
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ color }) => <FontAwesome5 name="wallet" size={24} color={color} />,
+        }}
+      />
+      </Tabs>
+  )
+}
