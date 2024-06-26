d6e8b9c52af3990578e166a6cdc3da7f3f05f7f9
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-06-25 20:00:29 -0400

    add balance and recent transactions to home screen

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextBase } from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text>HomeScreen</Text>
+    <View className='flex-1 items-center justify-center gap-4'>
+      <View className='bg-green-500 h-36 w-96 px-6 rounded justify-center'>
+        <Text className='text-center'>$(Balance)</Text>
+      </View>
+      <View className=' bg-gray-400 h-56 w-96 px-6 items-center rounded'>
+        <Text>Recent Transactions</Text>
+      </View>
     </View>
   )
 }
