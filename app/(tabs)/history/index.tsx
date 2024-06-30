e3a979203656ae8c2dd01b088ada97aa040dbbee
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-06-30 17:10:20 -0400

    add working Plaid Link (in History Screen)

--- app/(tabs)/history/index.tsx
+++ app/(tabs)/history/index.tsx
@@ -1,11 +1,16 @@
 import {View, Text} from 'react-native'
 import React from 'react'
+import PlaidLink from '@/lib/PlaidLink'
+
 
 const HistoryScreen = () => {
   return (
-    <View>
-      <Text>History Screen</Text>
-    </View>
+    <PlaidLink
+      linkToken={"link-sandbox-..."}
+      onEvent={(event) => console.log(event)}
+      onExit={(exit) => console.log(exit)}
+      onSuccess={(success) => console.log(success)}
+    />
   )
 }
 
