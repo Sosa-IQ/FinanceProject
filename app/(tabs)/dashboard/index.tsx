4ecab780c588fb5712ada3a6a8d0f4cb4d1c8fc7
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-07-15 15:43:26 -0400

    add button that opens Plaid Link

--- app/(tabs)/dashboard/index.tsx
+++ app/(tabs)/dashboard/index.tsx
@@ -1,30 +1,38 @@
-import { View, Text, TextBase, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button, Modal } from 'react-native'
+import React, { useState } from 'react'
 import { createLinkToken } from '@/lib/actions/user.actions';
 import BaseBackground from '@/components/BaseBackground';
+import PlaidLink from '@/components/PlaidLink';
 
 const DashboardScreen = () => {
-  // const user: User = {
-  //   $id: "01234",
-  //   email: "johndoe@example.com",
-  //   userId: "12345",
-  //   dwollaCustomerUrl: "https://api-sandbox.dwolla.com/customers/12345",
-  //   dwollaCustomerId: "12345",
-  //   firstName: "John",
-  //   lastName: "Doe",
-  //   name: "John Doe",
-  //   address1: "123 Main St",
-  //   city: "New York",
-  //   state: "NY",
-  //   postalCode: "10001",
-  //   dateOfBirth: "1990-01-01",
-  //   ssn: "123-45-6789",
-  // };
+  const user: User = {
+    $id: "01234",
+    email: "johndoe@example.com",
+    userId: "12345",
+    dwollaCustomerUrl: "https://api-sandbox.dwolla.com/customers/12345",
+    dwollaCustomerId: "12345",
+    firstName: "John",
+    lastName: "Doe",
+    name: "John Doe",
+    address1: "123 Main St",
+    city: "New York",
+    state: "NY",
+    postalCode: "10001",
+    dateOfBirth: "1990-01-01",
+    ssn: "123-45-6789",
+  };
 
-  // const handleCreateLinkToken = () => {
-  //   const linkToken = createLinkToken(user);
-  //   console.log(linkToken);
-  // };
+  const handleCreateLinkToken = () => {
+    const linkToken = createLinkToken(user);
+    console.log(linkToken);
+  };
+
+  const [modalVisible, setModalVisible] = useState(false);
+  
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
   return (
     <BaseBackground>
     <View className='flex-1 items-center justify-center gap-4'>
@@ -34,8 +42,28 @@ const DashboardScreen = () => {
       <View className=' bg-gray-400 h-56 w-96 px-6 items-center rounded'>
         <Text>Recent Transactions</Text>
       </View>
-      {/* <Button title="Create Link Token" onPress={handleCreateLinkToken} /> */}
+      <View className='bg-greenSecondary rounded-xl'>
+      <Button title="Connect Bank" onPress={toggleModal} />
+      </View>
     </View>
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={toggleModal}
+      >
+        <View className='flex-1 mt-24'>
+          <PlaidLink
+          // linkToken={String(linkToken)}
+          onEvent={(event) => console.log(event)}
+          onExit={(exit) => {
+            console.log(exit);
+            toggleModal();
+          }}
+          onSuccess={(success) => console.log(success)}
+          />
+        </View>
+      </Modal>
     </BaseBackground>
   )
 }
